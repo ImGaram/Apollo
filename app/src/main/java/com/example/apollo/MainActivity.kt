048146ed7e6c73e9ba5e8cb29aef36a8bd41d517
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-10-23 15:03:20 +0900

    android apollo 예제 공부

--- app/src/main/java/com/example/apollo/MainActivity.kt
+++ app/src/main/java/com/example/apollo/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.apollo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.apollographql.apollo3.ApolloClient
+import com.example.graphql.ResultQuery
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+// ./gradlew downloadApolloSchema --endpoint="https://rickandmortyapi.com/graphql" --schema="app/src/main/graphql/com/simpli/graphql/schema.json"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("https://rickandmortyapi.com/graphql/")
+            .build()
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            val result = apolloClient.query(ResultQuery()).execute()
+            val text = findViewById<TextView>(R.id.text_view)
+            text.text = result.data.toString()
+        }
     }
 }
